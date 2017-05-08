9fb1b2f22311bc5827424fc55a078b0a1554e182
foolishchow <ningjingsiyuan1990@163.com>
2017-05-08 15:04:18 +0800

    update codes

---
+++ scripts/vue.webpack.config.js
@@ -0,0 +1,84 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+// var StringReplacePlugin = require("string-replace-webpack-plugin");
+module.exports = {
+    entry: {
+        app:'./src/index.js'
+    },
+    watch: false,
+    output: {
+        path: path.resolve(__dirname, '../app/assets/'),
+        publicPath: '../assets/',
+        filename: '[name].js',
+        pathinfo: true,
+        chunkFilename: 'modules/[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /router\.yaml$/,
+                loader: 'vue-router-loader-yaml'
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        css: ExtractTextPlugin.extract({
+                            use: ['css-loader', 'postcss-loader'],
+                            fallback: 'vue-style-loader'
+                        })
+                    }
+                }
+            }, {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                // exclude: /node_modules/,
+            }, {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }, {
+                test: /\.css/,
+                loader: ExtractTextPlugin.extract({
+                        use: ['css-loader', 'postcss-loader'],
+                        fallback: 'vue-style-loader'
+                    })
+            },{
+                test: /\.yaml$/,
+                exclude: /router\.yaml$/,
+                loader: 'json-loader!yaml-loader'
+            }]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: '[name].css',
+            allChunks: true,
+        })
+        ,new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm.js',
+            'vue-source': path.resolve(__dirname, ''),
+            'erp-utils': path.resolve(__dirname, 'src/utils'),
+            'erp-components': path.resolve(__dirname, 'src/components')
+        }
+    },
+    performance: {
+        hints: false
+    },
+    stats: {
+        // Nice colored output
+        colors: true
+    },
+    devtool: 'none'
+}
+
